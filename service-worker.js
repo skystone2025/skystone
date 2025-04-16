6f574acfb7626c8f6af8ca9fabd3b826550861bd
skystone2025 <timlee957@gmail.com>
2025-04-17 01:54:50 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,15 @@
 
-const CACHE_NAME = 'sky-stone-cache-v2';
-const urlsToCache = [
-  './index.html',
-  './manifest.json',
-  './service-worker.js',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(name => {
-          if (name !== CACHE_NAME) return caches.delete(name);
-        })
-      );
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("skystone").then(cache => {
+      return cache.addAll(["index.html", "manifest.json"]);
     })
   );
-  return self.clients.claim();
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).then(response => {
-      return caches.open(CACHE_NAME).then(cache => {
-        cache.put(event.request, response.clone());
-        return response;
-      });
-    }).catch(() => caches.match(event.request))
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
   );
 });
