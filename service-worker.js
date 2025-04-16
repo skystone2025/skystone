86b23e3cbb5e9485a55f38b48e1f1ddf13d38f6d
skystone2025 <timlee957@gmail.com>
2025-04-16 12:08:19 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,40 @@
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('feed-cache').then(function(cache) {
-      return cache.addAll([
-        './feed.html',
-        './manifest.json',
-        './icon-192.png',
-        './icon-512.png'
-      ]);
-    })
+const CACHE_NAME = 'sky-stone-cache-v2';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './service-worker.js',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) return caches.delete(name);
+        })
+      );
     })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).then(response => {
+      return caches.open(CACHE_NAME).then(cache => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch(() => caches.match(event.request))
   );
 });
