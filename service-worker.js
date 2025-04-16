0ab7da3f8fb9a67885c0a9149e4b8ad453afe818
skystone2025 <timlee957@gmail.com>
2025-04-17 02:46:17 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,19 @@
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('skystone-v3.4.2').then(function(cache) {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json'
-      ]);
-    })
-  );
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('v3.4.1').then(cache => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './manifest.json'
+            ]);
+        })
+    );
 });
-
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
 });
