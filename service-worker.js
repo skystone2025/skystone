4188da99339179e8c9838b5710bf727801247ced
skystone2025 <timlee957@gmail.com>
2025-04-17 20:59:54 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,13 @@
-// Service worker placeholder
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('sky-stone-v342').then(cache =>
+      cache.addAll(['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'])
+    )
+  );
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
