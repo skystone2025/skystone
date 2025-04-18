9250cd67e74a1030c7b7675174e01423f9a8ecfa
skystone2025 <timlee957@gmail.com>
2025-04-18 20:34:05 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,7 @@
-// Service Worker Placeholder
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(self.skipWaiting());
+});
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
